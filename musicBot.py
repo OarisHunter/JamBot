69c71a239447886dca993d93cb73ff8e5b2390a1
Pierce Thompson <pthomp131@gmail.com>
2021-09-14 17:31:44 -0400

    Crude youtube stream implementation

--- musicBot.py
+++ musicBot.py
@@ -28,6 +28,8 @@ intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=bot_prefix, intents=intents)
 
 
+TEMP_LINK = "https://www.youtube.com/watch?v=zHtcvQAI000"
+
 @bot.event
 async def on_ready():
     try:
@@ -54,6 +56,32 @@ async def play_(ctx):
         return await ctx.channel.send("Not in a Voice Channel", delete_after=10)
 
     await ctx.message.delete()
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    ffmpeg_options = {
+        'options': '-vn',
+        "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        song_info = ydl.extract_info(TEMP_LINK, download=False)
+
+    # print(song_info)
+
+    vc.play(discord.FFmpegPCMAudio(song_info["formats"][0]["url"], **ffmpeg_options))
+    vc.source = discord.PCMVolumeTransformer(vc.source)
+    vc.volume = 1
+
+    while vc.is_playing():
+        await asyncio.sleep(1)
 
 
 @bot.command(name= 'disconnect', help= 'Disconnects from voice')
