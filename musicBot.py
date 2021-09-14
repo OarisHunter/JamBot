d2e03303e248be833318a562d6cdc55f3a69fc39
Pierce Thompson <pthomp131@gmail.com>
2021-09-14 19:48:22 -0400

    Expanded config
    Added Basic response messages

--- musicBot.py
+++ musicBot.py
@@ -8,6 +8,7 @@ Music Bot
 @author: Pierce Thompson
 """
 
+import ast
 import os
 import discord
 import asyncio
@@ -23,6 +24,8 @@ config_object.read("config.ini")
 bot_settings = config_object["BOT_SETTINGS"]
 bot_prefix = bot_settings["prefix"]
 test_song = bot_settings["test_song"]
+ydl_opts = ast.literal_eval(bot_settings["ydl_opts"])
+ffmpeg_opts = ast.literal_eval(bot_settings["ffmpeg_opts"])
 
 # Create member vars
 song_queue = []
@@ -32,20 +35,6 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix=bot_prefix, intents=intents)
-
-ydl_opts = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-}
-
-ffmpeg_options = {
-        'options': '-vn',
-        "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
-    }
 
 
 @bot.event
@@ -63,30 +52,46 @@ async def on_ready():
 
 
 @bot.command(name= 'play', help= 'Connects bot voice')
-async def play_(ctx):
-    if ctx.author.voice is not None:
-        try:
-            vc = await ctx.author.voice.channel.connect()
-        except discord.DiscordException:
-            vc = ctx.guild.voice_client
-    else:
-        await ctx.message.delete()
-        return await ctx.channel.send("Not in a Voice Channel", delete_after=10)
-
+async def play_(ctx, link):
     await ctx.message.delete()
 
+    # Check that author is in a voice channel
+    if ctx.author.voice is not None:
+        try:
+            # Connect to channel of author
+            vc = await ctx.author.voice.channel.connect()
+        except discord.DiscordException:
+            # Catch error if already connected
+            vc = ctx.guild.voice_client
+    else:
+        return await ctx.channel.send("Not in a Voice Channel", delete_after=10)
+
+    # Call Youtube_DL to fetch song info
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        song_info = ydl.extract_info(test_song, download=False)
+        # DEBUG COMMAND
+        if link == "DEBUG":
+            song_info = ydl.extract_info(test_song, download=False)
+        else:
+            song_info = ydl.extract_info(link, download=False)
 
-    song_queue.append(song_info)
+    title = song_info['title']
+    url = song_info["formats"][0]["url"]
 
-    await play_music_(ctx, vc)
+    # Add song to queue
+    if song_queue:
+        await ctx.channel.send(f"{title} added to Queue", delete_after=20)
+    song_queue.append((title, url))
+
+    # Play song if not playing a song
+    if not vc.is_playing():
+        await play_music_(ctx, vc)
 
 
 @bot.command(name= 'disconnect', help= 'Disconnects from voice')
 async def disconnect_(ctx):
     vc = ctx.guild.voice_client
 
+    # Check that the bot is connected to voice
     if vc.is_connected():
         await vc.disconnect()
         vc = None
@@ -100,16 +105,21 @@ async def play_music_(ctx, vc):
         try:
             if vc.is_connected() and not vc.is_playing():
                 # Create FFmpeg audio stream, attach to voice client
-                vc.play(discord.FFmpegPCMAudio(song_queue[0]["formats"][0]["url"], **ffmpeg_options))
+                vc.play(discord.FFmpegPCMAudio(song_queue[0][1], **ffmpeg_opts))
                 vc.source = discord.PCMVolumeTransformer(vc.source)
                 vc.volume = 1
+
+                # Display now playing message
+                print(f"Now Playing {song_queue[0][0]} in {ctx.author.voice.channel.name} of {ctx.guild.name}")
+                await ctx.channel.send(f"Now Playing {song_queue[0][0]}", delete_after=20)
+
         except discord.errors.ClientException:
             print(f"ClientException: Failed to Play Song in {ctx.guild.name}")
             break
 
-        song_queue.pop(0)
-
         while vc.is_playing():
             await asyncio.sleep(1)
+
+        song_queue.pop(0)
 
 bot.run(TOKEN)
--- musicBotConfigGenerator.py
+++ musicBotConfigGenerator.py
@@ -10,7 +10,9 @@ config_object = ConfigParser()
 
 config_object['BOT_SETTINGS'] = {
     "prefix": "~",
-    "test_song": "https://www.youtube.com/watch?v=zHtcvQAI000"
+    "test_song": "https://www.youtube.com/watch?v=zHtcvQAI000",
+    "ydl_opts": "{'format': 'bestaudio/best', 'restrictfilenames': True, 'nocheckcertificate': True, 'ignoreerrors': False, 'logtostderr': False, 'quiet': True, 'no_warnings': True, 'default_search': 'auto', 'source_address': '0.0.0.0', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192',}],}",
+    "ffmpeg_opts": "{'options': '-vn', 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'}"
 }
 
 with open('config.ini', 'w') as conf:
