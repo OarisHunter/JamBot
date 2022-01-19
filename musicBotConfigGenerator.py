ff4c9c475a95c3d490fbd873efd0c2ad617de60d
Pierce Thompson <pthomp131@gmail.com>
2022-01-19 09:12:56 -0500

    Added doom playlist link to config generator

--- musicBotConfigGenerator.py
+++ musicBotConfigGenerator.py
@@ -12,7 +12,7 @@ config_object = ConfigParser()
 
 config_object['BOT_SETTINGS'] = {
     "invite_link": "https://discord.com/api/oauth2/authorize?client_id=887369663724519444&permissions=8&scope=bot",
-    "test_song": "https://www.youtube.com/watch?v=zHtcvQAI000",
+    "doom_playlist": "https://www.youtube.com/playlist?list=PLg8__X57j6ugI1FOZbBvO-T3gBIwRS7GX",
     "ydl_opts": "{'format': 'bestaudio/best', 'restrictfilenames': True, 'nocheckcertificate': True, 'ignoreerrors': False, 'logtostderr': False, 'quiet': True, 'no_warnings': True, 'cookiefile': 'cookies.txt', 'default_search': 'auto', 'source_address': '0.0.0.0', 'postprocessors': [{'key': 'FFmpegExtractAudio', 'preferredcodec': 'mp3', 'preferredquality': '192',}],}",
     "ffmpeg_opts": "{'options': '-vn', 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'}",
     "embed_theme": "0xc27c0e",
